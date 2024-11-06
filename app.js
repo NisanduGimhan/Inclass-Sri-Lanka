9ca0afd49a1ec92d1f9966a6d07e88bcb0217b64
NisanduGimhan <nisanduab@gmail.com>
2024-11-06 16:57:05 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,82 @@
+loadItems();
+
+async function loadItems() {
+    
+    let res = await fetch("https://restcountries.com/v3.1/all");
+    let items = await res.json();
+    let body = "";
+    items.forEach(element => {
+        console.log(element);
+        body+=`
+            <div class="col">
+                <div class="card shadow-sm">
+                  <img src=${element.flags.png} class = "mx-auto " width =200px height=100px alt="">
+                  <div class="card-body">
+                  <h2>${element.name.common}</h2>
+                    <p class="card-text"> capital : ${element.capital}</p>
+                    <p class="card-text"> Region : ${element.region}</p>
+                    <p class="card-text"> Population : ${element.population}</p>
+                   
+                    <div>
+                      <div>
+                       
+                        <button type="button" class="btn btn-primary my-2"">View On google</button>
+                        
+                       
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        `;
+
+        
+        
+    });
+
+    console.log(body);
+
+    document.getElementById("row").innerHTML=body;
+    
+}
+
+function searchCountry(){
+    console.log("Search!!");
+    let txtSearch = document.getElementById("txtSearch").value;
+
+    fetch(`https://restcountries.com/v3.1/name/${txtSearch}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        let body="";
+        data.forEach(element => {
+            body+=`
+            
+           <div class="col">
+                <div class="card shadow-sm">
+                  <img src=${element.flags.png} class = "mx-auto " width =200px height=100px alt="">
+                  <div class="card-body">
+                  <h2>${element.name.common}</h2>
+                    <p class="card-text"> capital : ${element.capital}</p>
+                    <p class="card-text"> Region : ${element.region}</p>
+                    <p class="card-text"> Population : ${element.population}</p>
+                   
+                    <div>
+                      <div>
+                       
+                        <button type="button" class="btn btn-primary my-2"">View On google</button>
+                        
+                       
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        `;
+        });
+
+        document.getElementById("row").innerHTML=body;
+        
+    })
+    
+}
